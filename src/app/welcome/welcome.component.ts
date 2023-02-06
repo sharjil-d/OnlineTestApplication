47641ead507cf90a16f3c5cdb571e697a3bc601b
sharjil-d <109900115+sharjil-d@users.noreply.github.com>
2023-02-06 14:57:42 +0530

    Add files via upload

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent {
+  @ViewChild('name') nameKey!:ElementRef;
+  startQuiz(){
+    localStorage.setItem("name",this.nameKey.nativeElement.value);
+  }
+
+}
